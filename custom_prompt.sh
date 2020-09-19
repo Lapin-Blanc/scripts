1859a1608d4b6a98048178bfa8a114efcc019917
root <root@vps632065.ovh.net>
2020-09-19 11:15:09 +0200

    initial commit

---
+++ custom_prompt.sh
@@ -0,0 +1,7 @@
+# put this into /etc/profiles.d/
+if [ \$(id -u) -eq 0 ];
+then # vous êtes root, invite en bleu
+    export PS1="\[\e[00;36m\]\A\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;34m\]\u\[\e[0m\]\[\e[00;33m\]@\[\e[0m\]\[\e[00;37m\]\h \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;33m\]\$\[\e[0m\]\[\e[00;37m\] \[\e[0m\]"
+else # si vous êtes utilisateur normal, invite en rouge
+    export PS1="\[\e[00;36m\]\A\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;31m\]\u\[\e[0m\]\[\e[00;33m\]@\[\e[0m\]\[\e[00;37m\]\h \[\e[0m\]\[\e[00;32m\]\w\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;33m\]\$\[\e[0m\]\[\e[00;37m\] \[\e[0m\]"
+fi
