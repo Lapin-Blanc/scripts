311ea4b81f0896d305cdc1eeae542b6f38478764
Fabien Toune <fabien.toune@gmail.com>
2020-11-18 14:34:17 +0100

    Update video2gif.sh

--- video2gif.sh
+++ video2gif.sh
@@ -6,3 +6,4 @@ video2gif() {
   ffmpeg -i "${1}" -i "${1}.png" -filter_complex "fps=${3:-10},scale=${2:-320}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${1}".gif
   rm "${1}.png"
 }
+# ffmpeg -i input.mp4 -filter_complex 'fps=10,scale=320:-1:flags=lanczos,split [o1] [o2];[o1] palettegen [p]; [o2] fifo [o3];[o3] [p] paletteuse' out.gif
